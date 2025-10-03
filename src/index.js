26dee6592e3247c43e8a2422284a56203cf1aca0
Teemo Tebest <teemo.tebest@gmail.com>
2025-10-03 16:03:51 +0200

    Made the titles possibility to have two

--- src/index.js
+++ src/index.js
@@ -12,6 +12,18 @@ if (containerTitle) {
   rootTitle.render(<AppTitle first={containerTitle.dataset.first} second={containerTitle.dataset.second} />);
 }
 
+const containerTitles = document.querySelectorAll('.app-root-2025-unctad16-title');
+
+containerTitles.forEach(container => {
+  const rootTitle = createRoot(container);
+  rootTitle.render(
+    <AppTitle
+      first={container.dataset.first}
+      second={container.dataset.second}
+    />
+  );
+});
+
 const containerHeader = document.getElementById('app-root-2025-unctad16-header');
 if (containerHeader) {
   const rootHeader = createRoot(containerHeader);
--- src/jsx/AppHeader.jsx
+++ src/jsx/AppHeader.jsx
@@ -6,7 +6,7 @@ function App() {
     <div className="app">
       <a href="/unctad16">
         <div className="app_header">
-          <div className="arrow-x bg-un-red row px-5 py-3 custom-banner" style={{ minHeight: '300px;' }}>
+          <div className="arrow-x bg-un-red row px-5 py-3 custom-banner" style={{ minHeight: '300px' }}>
             <div className="content-wrapper">
               <div className="col-12 col-xl-2 g-0 ps- logo-container">
                 <img src="/sites/default/files/2025-09/unctad-16-logo-white.png" data-entity-uuid="18237496-2ea6-4c78-abca-3ac10e353be3" data-entity-type="file" alt="16th UN Conference on Trade and Development logo" />
